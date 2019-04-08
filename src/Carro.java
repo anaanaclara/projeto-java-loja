91da92addbd86929a8987913825573ce2b13cc9d
anaclara231 <ana.ribeiro284@etec.sp.gov.br>
2019-04-08 20:19:56 -0300

    Loja de carros Ana

---
+++ src/Carro.java
@@ -0,0 +1,24 @@
+
+public class Carro {
+    
+    String nome = "Palio";
+    String marca = "Fiat";
+    String cor = "Cinza";
+    String largura = "1.5";
+    String peso = "2000"; 
+    
+public void acelerar () {
+    System.out.println("Carro acelerando...");
+}
+
+public void acionarTração () {
+       
+}
+
+public void freiar () {
+    
+}
+
+public void acionarSeta () {
+    
+}}
---
+++ src/ClassePrincipal.java
@@ -0,0 +1,35 @@
+
+public class ClassePrincipal {
+    
+        public static void main (String[] args) {
+    Carro carro1 = new Carro (); {   
+    carro1.nome = "Palio";
+    carro1.marca = "Fiat";
+    carro1.cor = "Cinza";
+    carro1.largura = "1.5";
+    carro1.peso = "2000";
+    
+    System.out.println ("nome    1   = "  +  carro1.nome);
+    System.out.println ("marca   1   = "  +  carro1.marca);
+    System.out.println ("cor     1   = "  +  carro1.cor);
+    System.out.println ("largura 1   = "  +  carro1.largura);
+    System.out.println ("peso    1   = "  +  carro1.peso);
+    
+    Carro carro2 = new Carro (); {   
+    carro2.nome = "corsa";
+    carro2.marca = "Chevrolet";
+    carro2.cor = "Preto";
+    carro2.largura = "1.5";
+    carro2.peso = "2100"; 
+    
+    System.out.println ("nome    2   = "  +  carro2.nome);
+    System.out.println ("marca   2   = "  +  carro2.marca);
+    System.out.println ("cor     2   = "  +  carro2.cor);
+    System.out.println ("largura 2   = "  +  carro2.largura);
+    System.out.println ("peso    2   = "  +  carro2.peso);
+    
+    }}}}
+        
+
+        
+
